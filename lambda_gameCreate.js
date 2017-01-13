bd2365964610c57c98f7add50e9df8bcadd70219
mzaradzki <mathieu.zaradzki@gmail.com>
2017-01-13 12:24:34 +0100

    create creation now stores the opponent id

--- lambda_gameCreate.js
+++ lambda_gameCreate.js
@@ -2,69 +2,95 @@ console.log('Loading event');
 var AWS = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
-    // TODO implement
+    
+    console.log(event.player_identity_id);
+    console.log(event.guest_facebook_id);
     
     var docClient = new AWS.DynamoDB.DocumentClient();
-    
-    function makeid() {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?*-+";
-        for(var i=0; i<12; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+
+    function gameCreate(player_identity_id, guest_identity_id) {
+        function makeid() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?*-+";
+            for(var i=0; i<12; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
         }
-        return text;
+        
+        var game_id = makeid();
+        
+        var start_stamp = Date.now();
+        console.log(start_stamp);
+        
+        var output = {
+            game_id: game_id,
+            start_stamp: start_stamp
+        };
+        
+        // board is 10x10 for padding reasons
+        var board = [   '0000000000',// padding
+                        '0000000000',
+                        '0000000000',
+                        '0000000000',
+                        '0000BW0000',
+                        '0000WB0000',
+                        '0000000000',
+                        '0000000000',
+                        '0000000000',
+                        '0000000000']; //padding
+        
+        var black_id, white_id;
+        if (Math.random()>0.5) {
+            black_id = player_identity_id;
+            white_id = guest_identity_id; 
+        }
+        else {
+            black_id = guest_identity_id;
+            white_id = player_identity_id;
+        }
+        
+        var params = {
+            TableName :'OthelloGames',
+            Item:{
+                'game_id': game_id,
+                'start_stamp': start_stamp,
+                'board': board,
+                'turn': 'B',
+                'black_player_identity_id': black_id,
+                'white_player_identity_id': white_id
+            }
+        };
+        docClient.put(params, function(err, data) {
+            if (err) {
+                console.log("Unable to add item: " + "\n" + JSON.stringify(err, undefined, 2));
+                callback("Unable to add item");
+            } else {
+                console.log("PutItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
+                callback(null, output);
+            }
+        });
+        //callback(null, output);
     }
-    
-    var game_id = makeid();
-    
-    var start_stamp = Date.now();
-    console.log(start_stamp);
-    
-    var output = {
-        game_id: game_id,
-        start_stamp: start_stamp
-    };
-    
-    // board is 10x10 for padding reasons
-    var board = [   '0000000000',// padding
-                    '0000000000',
-                    '0000000000',
-                    '0000000000',
-                    '0000BW0000',
-                    '0000WB0000',
-                    '0000000000',
-                    '0000000000',
-                    '0000000000',
-                    '0000000000']; //padding
-    
-    var black_id, white_id;
-    if (Math.random()>0.5) {
-        black_id = event.player_identity_id;
-        white_id = "guest"; 
-    }
-    else {
-        black_id = "guest";
-        white_id = event.player_identity_id;
-    }
-    
+
     var params = {
-        TableName :'OthelloGames',
-        Item:{
-            'game_id': game_id,
-            'start_stamp': start_stamp,
-            'board': board,
-            'turn': 'B',
-            'black_player_identity_id': black_id,
-            'white_player_identity_id': white_id
+        TableName: 'OthelloPlayers',
+        Key: {
+            'facebook_id': event.guest_facebook_id
         }
     };
-    docClient.put(params, function(err, data) {
+    
+    docClient.get(params, function(err, data) {
         if (err) {
-            console.log("Unable to add item: " + "\n" + JSON.stringify(err, undefined, 2));
+            console.log("Unable to read item: " + "\n" + JSON.stringify(err, undefined, 2));
+            callback("Unable to read item");
         } else {
-            console.log("PutItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
+            console.log("GetItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
+            var guest_identity_id = data.Item.player_identity_id;
+            console.log(guest_identity_id);
+            gameCreate(event.player_identity_id, guest_identity_id);
         }
     });
     
-    callback(null, output);
 };
